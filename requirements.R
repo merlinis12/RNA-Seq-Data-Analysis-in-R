7b0a492c11116e7f485e35db597ad5d5db9451f1
merlinis12 <144267044+merlinis12@users.noreply.github.com>
2024-11-05 08:53:42 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,1 +1,5 @@
-#R packages required for the workshop
+# Install necessary packages
+install.packages(c("tidyverse", "ggplot2"))
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(c("DESeq2", "edgeR"))
