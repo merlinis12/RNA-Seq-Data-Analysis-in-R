84d19a7fc3259d774d1d25c1560830086d8e1a76
merlinis12 <144267044+merlinis12@users.noreply.github.com>
2024-11-08 09:45:45 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -2,4 +2,8 @@
 install.packages(c("tidyverse", "ggplot2"))
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install(c("DESeq2", "edgeR", "limma"))
+BiocManager::install(c("DESeq2", "edgeR", "limma", "AnnotationDbi", "org.Hs.eg.db"))
+
+
+if (!requireNamespace("pheatmap", quietly = TRUE)) install.packages("pheatmap")
+
